d061e26aa670ba04fe8efdb53ed929445f21e93a
rafa-kohlhoff <rafaelpia96@hotmail.com>
2025-07-29 17:58:27 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+print("setup Start")
+window = pygame.display.set_mode(size=(600, 480))
+print("Loop Start")
+while True:
+    # Check for all evevnts
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()  # end pygame
