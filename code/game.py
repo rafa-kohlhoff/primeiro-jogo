59e43004892d44734b7689d0f71258df01c2ae87
rafa-kohlhoff <rafaelpia96@hotmail.com>
2025-08-02 16:00:34 -0300

    commit menu img, sons

--- code/game.py
+++ code/game.py
@@ -1,21 +1,18 @@
 import pygame
 
-from code.menu import Menu
+from code.Menu import Menu
+from code.const import WIN_WIDTH, WIN_HEIGHT
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-    # Check for all events
-    # for event in pygame.event.get():
-    # if event.type == pygame.QUIT:
-    # pygame.quit()
-    # quit()  # end pygame
