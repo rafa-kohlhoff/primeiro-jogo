c1fe7228ded908d71679703977542ca3779877f9
rafa-kohlhoff <rafaelpia96@hotmail.com>
2025-08-02 16:00:34 -0300

    commit menu img, sons

---
+++ code/const.py
@@ -0,0 +1,14 @@
+from pygame.examples.grid import WINDOW_HEIGHT
+#C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('NEW GAME 1P'
+               'NEW GAME 2P - COOPERATIVE'
+               'NEW GAME 2P - COMPETITIVE'
+               'SCORE'
+               'EXITE')
+#W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
